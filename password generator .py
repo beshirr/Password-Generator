91de8a82bd1be2dc5cf1a42043da811e376f250f
Youssef Ahmed Beshir <154796903+beshirr@users.noreply.github.com>
2024-04-25 00:33:55 +0200

    Add files via upload

---
+++ password generator .py
@@ -0,0 +1,26 @@
+import random
+from string import ascii_letters, digits, punctuation
+
+
+def password_generator():
+    password = ""
+    chars = []
+
+    for i in range(8):
+        chars.append(random.choice(ascii_letters))
+
+    for i in range(4):
+        chars.append(random.choice(digits))
+
+    for i in range(4):
+        chars.append(random.choice(punctuation))
+
+    for i in range(16):
+        password += random.choice(chars)
+
+    return password
+
+
+print("Welcome to the password generator")
+created_password = password_generator()
+print(f'Your password id {created_password}')
